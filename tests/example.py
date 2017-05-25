81bb681ae8f3630c548b299ba6cc80460bfd1af6
pawel-lewtak <lewtak@gmail.com>
2017-05-25 09:56:57 +0200

    Add example of class import and how to run tests

--- tests/example.py
+++ tests/example.py
@@ -1,9 +1,15 @@
 import unittest
+from src.dojo import Dojo
+
+"""
+Call "python -m tests.example" while being in parent directory to run tests in this file.
+"""
 
 
 class ExampleTest(unittest.TestCase):
     def test_example(self):
-        self.assertEqual(1, 1)
+        dojo = Dojo()
+        self.assertEqual(dojo.get_random_number(), 4)
 
 if __name__ == '__main__':
     unittest.main()
